72e0edbeaaf47be4819fa80f8e954adc8f6bd0f4
iamsiamhossen <siamhossencse29@gmail.com>
2025-02-01 23:17:47 +0600

    first commit

---
+++ B_Clockwork.cpp
@@ -0,0 +1,53 @@
+/**
+ * Author: iamsiamhossen
+ * Created: 01-02-2025 23:08:20
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+// fastread
+#define fastread() (ios_base::sync_with_stdio(0), cin.tie(0))
+// Shortcut
+#define endl "\n"
+#define int long long
+#define float double
+#define all(X) (X).begin(), (X).end()
+#define Reverse(X) reverse(All(X))
+#define Unique(X) (X).erase(unique((X).begin(), (X).end()), (X).end())
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define Yes cout << "Yes\n"
+#define No cout << "No\n"
+// MOD
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define INF 1001001001
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] <= 2 * max(i, n - i - 1))
+        {
+            cout << "NO" << endl;
+            return;
+        }
+    }
+    cout << "YES" << endl;
+}
+int32_t main()
+{
+    fastread();
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++)
+    {
+        // cout << "Case " << t << ": ";
+        solve();
+    }
+    return 0;
+}
