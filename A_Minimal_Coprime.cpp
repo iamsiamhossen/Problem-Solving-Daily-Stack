2dd112d1eb6a33862e6d3549dc52ff3fc43bf07c
iamsiamhossen <siamhossencse29@gmail.com>
2025-02-03 23:39:11 +0600

    New Commit

---
+++ A_Minimal_Coprime.cpp
@@ -0,0 +1,51 @@
+/**
+ * Author: iamsiamhossen
+ * Created: 03-02-2025 23:37:39
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+// fastread
+#define fastread() (ios_base::sync_with_stdio(0), cin.tie(0))
+// Shortcut
+#define endl "\n"
+#define int long long
+#define float double
+#define all(X) (X).begin(), (X).end()
+#define Reverse(X) reverse(All(X))
+#define Unique(X) (X).erase(unique((X).begin(), (X).end()), (X).end())
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define Yes cout << "Yes\n"
+#define No cout << "No\n"
+// MOD
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define INF 1001001001
+void solve()
+{
+    int l, r;
+    cin >> l >> r;
+    int tmp = __gcd(l, r);
+    if (l == r)
+    {
+        if (tmp == 1)
+            cout << 1 << endl;
+        else
+            cout << 0 << endl;
+    }
+    else
+        cout << r - l << endl;
+}
+int32_t main()
+{
+    fastread();
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++)
+    {
+        // cout << "Case " << t << ": ";
+        solve();
+    }
+    return 0;
+}
